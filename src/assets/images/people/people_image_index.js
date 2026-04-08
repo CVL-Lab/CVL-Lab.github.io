3dd798fb76d4e972456958767f30341ee12b46d6
gnaroshi <gnaroshi@gmail.com>
2026-04-08 12:11:03 +0900

    Update Interns and M.S

--- src/assets/images/people/people_image_index.js
+++ src/assets/images/people/people_image_index.js
@@ -1,7 +1,7 @@
 // professor
 import JongbinRyu from "./Jongbin Ryu.png";
 
-// intergrated master and phd
+// integrated master and phd
 import SungeunKim from "./Sungeun Kim.jpg";
 import JunhyeongKo from "./Junhyeong Ko.jpg";
 import HankyulKang from "./Hankyul Kang.jpg";
@@ -19,12 +19,11 @@ import DonghyunKim from "./Donghyun Kim.jpg";
 import DonggunLee from "./Donggun Lee.png";
 import EuihakHeo from "./Euihak Heo.png";
 import EunjuShin from "./Eunju Shin.png";
+import DuyeonNa from "./Duyeon Na.png";
+import MinjaeKim from "./Minjae Kim.jpg";
 
 // intern
-import DuyeonNa from "./Duyeon Na.png";
 import GeunhyungPark from "./Geunhyung Park.png";
-import HwasikJeong from "./Hwasik Jeong.jpg";
-import MinjaeKim from "./Minjae Kim.jpg";
 
 //alumni
 
@@ -34,41 +33,41 @@ import HyunsooPark from "./Hyunsoo Park.jpg";
 import InhyukPark from "./Inhyuk Park.jpg";
 
 const people_images = {
-  professor: {
-    jonbin_ryu: JongbinRyu,
-  },
-  intergrated_mp: {
-    sungeun_kim: SungeunKim,
-    junhyeong_ko: JunhyeongKo,
-    hankyul_kang: HankyulKang,
-    seongmin_oh: SeongminOh,
-  },
-  phd: {},
-  master: {
-    sunki_joo: SunkiJoo,
-    soohyeon_pyo: SoohyeonPyo,
-    yuri_kim: YuriKim,
-    gregor_seifer: GregorSeifer,
-    seunghun_kang: SeunghunKang,
-    dongkyu_kim: DongkyuKim,
-    kyusung_nam: KyusungNam,
-    donghyun_kim: DonghyunKim,
-    donggun_lee: DonggunLee,
-    euihak_heo: EuihakHeo,
-    eunju_shin: EunjuShin,
-  },
-  intern: {
-    duyeon_na: DuyeonNa,
-    geunhyung_park: GeunhyungPark,
-    hwasik_jeong: HwasikJeong,
-    minjae_kim: MinjaeKim,
-  },
-  alumni: {
-    seokjin_jang: SeokjinJang,
-    jihoon_kim: JihoonKim,
-    hyunsoo_park: HyunsooPark,
-    inhyuk_park: InhyukPark,
-  },
+    professor: {
+        jonbin_ryu: JongbinRyu,
+    },
+    integrated_mp: {
+        sungeun_kim: SungeunKim,
+        junhyeong_ko: JunhyeongKo,
+        hankyul_kang: HankyulKang,
+        seongmin_oh: SeongminOh,
+    },
+    phd: {},
+    master: {
+        sunki_joo: SunkiJoo,
+        soohyeon_pyo: SoohyeonPyo,
+        yuri_kim: YuriKim,
+        gregor_seifer: GregorSeifer,
+        seunghun_kang: SeunghunKang,
+        dongkyu_kim: DongkyuKim,
+        kyusung_nam: KyusungNam,
+        donghyun_kim: DonghyunKim,
+        donggun_lee: DonggunLee,
+        euihak_heo: EuihakHeo,
+        eunju_shin: EunjuShin,
+    },
+    intern: {
+        duyeon_na: DuyeonNa,
+        geunhyung_park: GeunhyungPark,
+        hwasik_jeong: HwasikJeong,
+        minjae_kim: MinjaeKim,
+    },
+    alumni: {
+        seokjin_jang: SeokjinJang,
+        jihoon_kim: JihoonKim,
+        hyunsoo_park: HyunsooPark,
+        inhyuk_park: InhyukPark,
+    },
 };
 
 export default people_images;
